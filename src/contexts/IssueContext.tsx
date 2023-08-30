9195fc51a4fbd26911f54c9f3893db9b7448fc5b
ukssss <1872003115su@gmail.com>
2023-08-30 14:33:40 +0900

    Feat | IssueContext ContextAPI 제작

---
+++ src/contexts/IssueContext.tsx
@@ -0,0 +1,53 @@
+import { getIssueDetail, getIssueList } from '@utils/fetchData';
+import React, { createContext, useState } from 'react';
+
+interface IssueContextProps {
+  issueList: IssueProps[];
+  issueDetail: IssueProps | null;
+  fetchIssueList: () => Promise<void>;
+  fetchIssueDetail: (issueNumber: number) => Promise<void>;
+}
+
+interface IssueProviderProps {
+  children: React.ReactNode;
+}
+
+const initialContext: IssueContextProps = {
+  issueList: [],
+  issueDetail: null,
+  fetchIssueList: async () => {},
+  fetchIssueDetail: async () => {},
+};
+
+export const IssueContext = createContext<IssueContextProps>(initialContext);
+
+export const IssueProvider = ({ children }: IssueProviderProps) => {
+  const [issueList, setIssueList] = useState([]);
+  const [issueDetail, setIssueDetail] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const fetchIssueList = async () => {
+    try {
+      const data = await getIssueList({ perPage: 10, page: page });
+      setPage(prev => prev + 1);
+      setIssueList(prev => [...prev, ...data]);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const fetchIssueDetail = async (issueNumber: number) => {
+    try {
+      const data = await getIssueDetail({ issueNumber: issueNumber });
+      setIssueDetail(data);
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  return (
+    <IssueContext.Provider value={{ issueList, issueDetail, fetchIssueList, fetchIssueDetail }}>
+      {children}
+    </IssueContext.Provider>
+  );
+};
