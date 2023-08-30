1cb6923ce65a0c61296d252a0d3d3037f732f4f9
ukssss <1872003115su@gmail.com>
2023-08-30 14:33:05 +0900

    Feat | fetchData 유틸 함수 제작
    - getIssueList, getIssueDetail

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,54 @@
+import { Octokit } from '@octokit/rest';
+
+export interface GetIssueListProps {
+  perPage: number;
+  page: number;
+}
+
+export interface GetIssueDetailProps {
+  issueNumber: number;
+}
+
+const ACCESS_TOKEN = process.env.REACT_APP_ACCESS_TOKEN;
+const OWNER_NAME = 'facebook';
+const REPO_NAME = 'react';
+
+const octokit = new Octokit({
+  auth: ACCESS_TOKEN,
+});
+
+export const getIssueList = async ({ perPage, page }: GetIssueListProps) => {
+  try {
+    const res = await octokit.request(
+      `GET /repos/${OWNER_NAME}/${REPO_NAME}/issues?per_page=${perPage}&page=${page}&sort=comments`,
+      {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      },
+    );
+
+    console.log('Issues: ', res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export const getIssueDetail = async ({ issueNumber }: GetIssueDetailProps) => {
+  try {
+    const res = await octokit.request(
+      `GET /repos/${OWNER_NAME}/${REPO_NAME}/issues/${issueNumber}`,
+      {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      },
+    );
+
+    console.log('IssueNumber: ', res.data);
+    return res.data;
+  } catch (err) {
+    throw err;
+  }
+};
