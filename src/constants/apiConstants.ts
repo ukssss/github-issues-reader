e7c4d9117cae94eeb49a843e47ad180c71a494cf
ukssss <1872003115su@gmail.com>
2023-09-01 21:34:46 +0900

    Refactor | apiConstant 변수 파일 분리

---
+++ src/constants/apiConstants.ts
@@ -0,0 +1,5 @@
+export const API_URL = `GET /repos/{owner}/{repo}/issues`;
+export const API_URL_DETAIL = `GET /repos/{owner}/{repo}/issues/{issue_number}`;
+export const PER_PAGE = 20;
+export const REPO = 'react';
+export const OWNER = 'facebook';
