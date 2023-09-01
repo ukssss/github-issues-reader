fac1f16a8c513a8ca0b82e889fb8b05bf7572772
ukssss <1872003115su@gmail.com>
2023-09-01 22:22:12 +0900

    Design | Header 컴포넌트 고정형 디자인 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { OWNER, REPO } from '@constants/apiConstants';
 import { Link } from 'react-router-dom';
 import { styled } from 'styled-components';
 
@@ -7,21 +7,39 @@ interface HeaderProps {}
 const Header = ({}: HeaderProps) => {
   return (
     <>
-      <StyledH1>
-        <StyledLink to="/">Facebook/React</StyledLink>
-      </StyledH1>
+      <StyledHeader>
+        <StyledContainer>
+          <StyledLink to="/">
+            {OWNER} / {REPO}
+          </StyledLink>
+        </StyledContainer>
+      </StyledHeader>
     </>
   );
 };
 
-const StyledH1 = styled.h1`
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  padding: 30px 0;
+  z-index: 1;
+`;
+
+const StyledContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
+
 const StyledLink = styled(Link)`
+  text-transform: capitalize;
   text-decoration: none;
   color: black;
+  font-size: xx-large;
+  font-weight: bold;
   cursor: pointer;
 `;
 
