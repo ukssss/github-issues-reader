75772a43f26e302b2237a195d0b9c3f4b05ab187
ukssss <1872003115su@gmail.com>
2023-09-01 22:38:03 +0900

    !HOTFIX | CSS 초기화로 인한 레이아웃 무너짐 수정 (IssueDetail)

--- src/components/IssueDetail/IssueDetail.tsx
+++ src/components/IssueDetail/IssueDetail.tsx
@@ -12,14 +12,14 @@ const IssueDetail = ({ issue }: IssueDetailProps) => {
     <>
       <StyledDiv>
         <StyledContainer>
-          <StyledTitle>title : {issue?.title}</StyledTitle>
+          <StyledTitle>{issue?.title}</StyledTitle>
           <StyledIssueNumber>#{issue?.number}</StyledIssueNumber>
           <StyledAvatar src={issue?.user.avatar_url} alt="avatar" />
-          <StyledWriter>작성자 : {issue?.user.login}</StyledWriter>
-          <StyledDate>만든날짜 : {issue?.created_at}</StyledDate>
+          <StyledWriter>작성자: {issue?.user.login}</StyledWriter>
+          <StyledDate>작성일: {issue?.created_at}</StyledDate>
           <StyledComments>코멘트 : {issue?.comments}</StyledComments>
         </StyledContainer>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{issue?.body}</ReactMarkdown>
+        <StyledViewer remarkPlugins={[remarkGfm]}>{issue?.body}</StyledViewer>
       </StyledDiv>
     </>
   );
@@ -28,32 +28,53 @@ const IssueDetail = ({ issue }: IssueDetailProps) => {
 const StyledDiv = styled.div`
   position: relative;
 `;
+
 const StyledContainer = styled.div`
   border-bottom: 1px solid #ccc;
   padding-bottom: 20px;
 `;
+
 const StyledTitle = styled.h3`
   position: absolute;
-  top: -20px;
+  top: 0;
   left: 120px;
+  font-size: 18px;
+  font-weight: bold;
 `;
+
 const StyledIssueNumber = styled.div`
   position: absolute;
+  top: 0;
   left: 60px;
+  font-size: 18px;
+  font-weight: bold;
 `;
+
 const StyledWriter = styled.span`
   margin-left: 10px;
 `;
+
 const StyledAvatar = styled.img`
   width: 50px;
   height: 50px;
 `;
+
 const StyledComments = styled.span`
   position: absolute;
+  top: 20px;
   right: 0;
 `;
+
 const StyledDate = styled.span`
-  margin-left: 5px;
+  margin-left: 10px;
+`;
+
+const StyledViewer = styled(ReactMarkdown)`
+  all: initial;
+
+  & * {
+    all: revert;
+  }
 `;
 
 export default IssueDetail;
