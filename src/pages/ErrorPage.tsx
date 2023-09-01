a27bcb644f68140f4884079a9f4c38df470321b3
ukssss <1872003115su@gmail.com>
2023-09-01 22:42:25 +0900

    Design | ErrorPage CSS 작업

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 const ErrorPage = () => {
-  return <div>Error</div>;
+  return (
+    <StyledContainer>
+      <StyledTitle>404 - 페이지를 찾을 수 없습니다.</StyledTitle>
+      <StyledLink to="/">홈으로 돌아가기</StyledLink>
+    </StyledContainer>
+  );
 };
+
+const StyledContainer = styled.div`
+  padding: 2rem;
+`;
+
+const StyledTitle = styled.h1`
+  margin-bottom: 5px;
+`;
+
+const StyledLink = styled(Link)``;
 
 export default ErrorPage;
